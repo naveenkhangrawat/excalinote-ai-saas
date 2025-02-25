9008c5690db75ec45778e42f0dbcb58b56471311
Naveen Khangrawat <khangrawat.naveen@gmail.com>
2025-02-25 21:24:18 +0530

    Add application banner image and update reference in ApplicationBanner component

--- src/app/(root)/_components/ApplicationBanner.tsx
+++ src/app/(root)/_components/ApplicationBanner.tsx
@@ -9,7 +9,7 @@ function ApplicationBanner() {
             <div className='w-[75%] md:blur-[100px] blur-[80px] rounded-full h-32 absolute bg-primary z-0 top-[30px]' />
             <div className="w-full max-w-[1250px] sm:w-[95%] rounded-xl p-4 ring-1 ring-gray-700/30 backdrop-blur-xl bg-black/5">
                 <div className='rounded-xl overflow-hidden'>
-                    <Image src={'/application-banner.png'} alt='Application banner' width={1250} height={600} className='w-full h-full object-cover' />
+                    <Image src={'/application-banner.jpeg'} alt='Application banner' width={1250} height={600} className='w-full h-full object-cover' />
                 </div>
             </div>
         </div>
